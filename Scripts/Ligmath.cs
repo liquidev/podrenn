19efb78ab8e73922538f62c59c24b8f201362e89
lqdev <liquidev@tutanota.com>
2022-05-17 14:40:34 +0200

    fix NaN

--- Scripts/Ligmath.cs
+++ Scripts/Ligmath.cs
@@ -4,8 +4,9 @@ public class Ligmath // Ligmath balls.
 {
    public static Vector3 Smid(Vector3 a, Vector3 b)
    {
-      if (a == b)
+      if (a.DistanceSquaredTo(b) <= 0.00001f)
          return a;
+      GD.Print(a, b);
       var om = a.AngleTo(b);
       var coeff = Mathf.Sin(0.5f * om) / Mathf.Sin(om);
       return coeff * (a + b);
